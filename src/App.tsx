7f93c465675cb9f06543a7e52a86faae3dc09ae3
Rahul Kundu <snrlab.in@gmail.com>
2021-08-06 11:24:05 +0530

    Adding Toast and Confirmation Dialog

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Switch, Redirect } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 
+import ContextProviders from './context-providers';
 import CustomTheme from "./theme/custom-theme";
 import Info from "./pages/Info";
 import Todo from "./pages/Todo";
@@ -18,13 +19,15 @@ function App() {
     <ApolloProvider client={client}>
       <CssBaseline />
       <ThemeProvider theme={CustomTheme}>
-        <Router>
-          <Switch>
-            <Route path="/info" exact component={Info} />
-            <Route path="/todo" component={Todo} />
-            <Redirect to="/todo"></Redirect>
-          </Switch>
-        </Router>
+        <ContextProviders>
+          <Router>
+            <Switch>
+              <Route path="/info" exact component={Info} />
+              <Route path="/todo" component={Todo} />
+              <Redirect to="/todo"></Redirect>
+            </Switch>
+          </Router>
+        </ContextProviders>
       </ThemeProvider>
     </ApolloProvider>
   );
